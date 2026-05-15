536ea2f16bd2c808570891fb3d5941903296c027
user <user@flywengMacBook-Air.local>
2026-05-15 16:49:38 +0800

    deploy: update pwa icon assets

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778833191|2227906';
+const CACHE_VERSION = '1778834952|2215592';
 /** @type {string} */
 const CACHE_PREFIX = 'AmaGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
