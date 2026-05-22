be798d31efcefdde32d0dd8cf549cc14c9624835
user <user@flywengMacBook-Air.local>
2026-05-22 09:10:26 +0800

    deploy: publish mobile tutorial card fix

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779355867|2426066';
+const CACHE_VERSION = '1779412187|2113405';
 /** @type {string} */
 const CACHE_PREFIX = 'AmaGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
