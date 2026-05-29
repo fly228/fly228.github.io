c9b1389ddc3914fe97e15032fbb2d9bc5c176319
user <user@Mac.internal.jingmi.com.tw>
2026-05-29 16:48:17 +0800

    Update loading screen layout

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779412187|2113405';
+const CACHE_VERSION = '1780044445|2815944';
 /** @type {string} */
 const CACHE_PREFIX = 'AmaGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
