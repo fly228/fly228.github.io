234140aebe787593c1602c5d95e4535c3e8fead9
user <user@Mac.internal.jingmi.com.tw>
2026-06-01 17:48:04 +0800

    Deploy shell-managed fullscreen scaling test

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780303028|3862428';
+const CACHE_VERSION = '1780307132|2554599';
 /** @type {string} */
 const CACHE_PREFIX = 'AmaGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
