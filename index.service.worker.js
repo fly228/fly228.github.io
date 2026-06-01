e54ca36038cb9fb0f8725f09d665cc13e9f32242
user <user@flywengMacBook-Air.local>
2026-06-01 08:59:22 +0800

    Export normal web build

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780044445|2815944';
+const CACHE_VERSION = '1780275520|2225212';
 /** @type {string} */
 const CACHE_PREFIX = 'AmaGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
