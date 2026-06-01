2a1da5ccc70978c35822d16dd0c8afd3940d70e3
user <user@Mac.internal.jingmi.com.tw>
2026-06-01 16:40:35 +0800

    Deploy stretch mode fullscreen test

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780299331|3500954';
+const CACHE_VERSION = '1780303028|3862428';
 /** @type {string} */
 const CACHE_PREFIX = 'AmaGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
