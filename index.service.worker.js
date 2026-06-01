4e9840b8270f26152bd33c60c33ef55dc3d9f271
user <user@Mac.internal.jingmi.com.tw>
2026-06-01 15:38:33 +0800

    Try mobile cover shell scaling

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780275520|2225212';
+const CACHE_VERSION = '1780299331|3500954';
 /** @type {string} */
 const CACHE_PREFIX = 'AmaGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
