fb674d40ac286f4619b49917cb3ca273a69ebb74
user <user@flywengMacBook-Air.local>
2026-06-02 14:40:15 +0800

    Lock PWA shell viewport after input

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780363027|2703434|shellfix20260602e';
+const CACHE_VERSION = '1780363027|2703434|shellfix20260602f';
 /** @type {string} */
 const CACHE_PREFIX = 'AmaGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
