c2c39c077e0c1bbae102f69e3c26bf01c6b0995e
user <user@flywengMacBook-Air.local>
2026-06-03 11:27:46 +0800

    Disable experimental web virtual keyboard

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780363027|2703434|shellfix20260603a';
+const CACHE_VERSION = '1780363027|2703434|shellfix20260603b';
 /** @type {string} */
 const CACHE_PREFIX = 'AmaGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
