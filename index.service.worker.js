d98f8a03680f66e1fed9ee99eb5da874b9f5c483
user <user@Mac.hitronhub.home>
2026-07-04 15:11:54 +0800

    deploy: export latest web build

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783068861|2471009';
+const CACHE_VERSION = '1783149086|2929008';
 /** @type {string} */
 const CACHE_PREFIX = 'AmaGame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
